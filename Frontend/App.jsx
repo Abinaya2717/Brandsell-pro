de0ca28cc64df4a66942a562920caa30149b02db
Abinaya <sabinaya133@gmail.com>
2026-03-03 12:40:44 +0000

    project

---
+++ Frontend/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Loginpage from './assets/pages/Loginpage'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Loginpage/>
+    </>
+  )
+}
+
+export default App
